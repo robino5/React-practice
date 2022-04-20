9866eaecc0d06809e4bab4d2a204307bc44dc7d0
Robin <anamulrobin05@gmail.com>
2022-04-20 22:25:47 +0600

    add shared component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Header from './components/header/Header';
 import Footer from './components/Footer/Footer'
 import Count from './components/body/Count'
 import  From from './components/body/Form'
+import Shared from './components/body/Shared'
 
 
 
 function App(){
 return   <div>
      <Header />
-     <From />
+     <Shared type="text"/>
+     <Shared type="password"/>
    <Footer />
 </div>
 }
---
+++ src/components/body/Shared.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Shared(props){
+    return(
+        <input type={props.type} name="name" />
+    )
+}
